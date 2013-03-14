e2fbf593ad61f0a951e0f0089045965437f6a5d1
Billy Mills <billymills@Billys-MacBook-Pro.local>
2013-03-13 21:27:36 -0400

    style changes, experimenting with twitter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   def home
   	require "rubygems"
 	require "twitter"
+	require "tweetstream"
+
 	Twitter.configure do |config|
 		config.consumer_key = Figaro.env.consumer_key
 		config.consumer_secret = Figaro.env.consumer_secret
@@ -10,9 +12,25 @@ class PagesController < ApplicationController
 	end
 
 	# pull three recent tweets from lady gaga
-	@tweets = Twitter.user_timeline("Mr_Hairston15", :count => 3, :result_type => "recent")
+	@tweets = Twitter.user_timeline("Mr_Hairston15", :count => 5, :result_type => "recent")
 	
 	# @twitter_feed = Twitter.user_timeline("ladygaga").first.text
+
+	# playing with streaming below
+	
+	# @statuses = []
+	# TweetStream.configure do |config|
+	#	config.consumer_key = Figaro.env.consumer_key
+	#	config.consumer_secret = Figaro.env.consumer_secret
+	#	config.oauth_token = Figaro.env.oauth_token
+	#	config.oauth_token_secret = Figaro.env.oauth_token_secret
+	# end
+	
+	# @client = TweetStream::Client.new.track('yankess') do |status|
+	#	@statuses << status.text
+	#	client.stop if @tweets.size >=5
+	# end
+	
 
   end
   	
