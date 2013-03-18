fb328e506b6dcf8e0ae0e1beb4a30b7d1374c2ec
Billy Mills <billymills@Billys-MacBook-Pro.local>
2013-03-18 17:59:05 -0400

    created basic Job model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130224011244) do
+ActiveRecord::Schema.define(:version => 20130318212649) do
+
+  create_table "Jobs", :force => true do |t|
+    t.string   "title"
+    t.string   "company"
+    t.string   "location"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
