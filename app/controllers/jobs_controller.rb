a831d7c651502d3e92550cb3b774eba03d19e384
Billy Mills <billymills@Billys-MacBook-Pro.local>
2013-03-19 20:31:38 -0400

    finished basic job submission

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,4 +7,20 @@ class JobsController < ApplicationController
   	def new
 		@job = Job.new
   	end
+
+	def create
+
+		# the following line is equivalent to
+		# Job.new(title: "example", company: "example", location: "example"
+		# it's a hash of hashes
+
+		@job = Job.new(params[:job])
+		if @job.save
+			flash[:success] = "This submission has just been tweeted!"
+			redirect_to @job
+			# I think this is where I should send the tweet from
+		else
+			render 'new'
+		end
+	end
 end
